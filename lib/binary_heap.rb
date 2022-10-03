61dabc9dff9212f145ff520c3566b18f19ed292b
Jared Echols <jechols@mavenlink.com>
2022-10-03 16:31:53 -0600

    ref: move methods up

--- lib/binary_heap.rb
+++ lib/binary_heap.rb
@@ -32,11 +32,32 @@ class BinaryHeap
   private
 
   def bubble_up
-    raise StandardError.new("Method missing: bubble_up")
+    position = heap_array.length - 1
+    parent_position = parent(position)
+    while position > 0 && compare(heap_array[position], heap_array[parent_position])
+      swap(position, parent_position)
+      position = parent_position
+      parent_position = parent(position)
+    end
   end
 
-  def is_leaf(position)
-    heap_array.length < left_child(position)
+  def bubble_down(position)
+    left = left_child(position)
+    right = right_child(position)
+    smallest = position
+
+    if left < heap_array.length && compare(heap_array[left], heap_array[position])
+      smallest = left
+    end
+
+    if right < heap_array.length && compare(heap_array[right], heap_array[smallest])
+      smallest = right
+    end
+
+    if smallest != position
+      swap(position, smallest)
+      bubble_down(smallest)
+    end
   end
 
   def swap(i, j)
