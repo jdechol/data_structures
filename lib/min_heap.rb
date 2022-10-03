87e6f02fbf0541d447bfd2e860ff84cf9e3d50cc
Jared Echols <jechols@mavenlink.com>
2022-10-03 16:30:38 -0600

    ref: introduce compare to min_heap

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -1,10 +1,12 @@
 require 'binary_heap'
 
 class MinHeap < BinaryHeap
+  COMPARATOR = '<'
+
   def bubble_up
     position = heap_array.length - 1
     parent_position = parent(position)
-    while position > 0 && heap_array[position] < heap_array[parent_position]
+    while position > 0 && compare(heap_array[position], heap_array[parent_position])
       swap(position, parent_position)
       position = parent_position
       parent_position = parent(position)
@@ -16,11 +18,11 @@ class MinHeap < BinaryHeap
     right = right_child(position)
     smallest = position
 
-    if left < heap_array.length && heap_array[left] < heap_array[position]
+    if left < heap_array.length && compare(heap_array[left], heap_array[position])
       smallest = left
     end
 
-    if right < heap_array.length && heap_array[right] < heap_array[smallest]
+    if right < heap_array.length && compare(heap_array[right], heap_array[smallest])
       smallest = right
     end
 
@@ -28,5 +30,9 @@ class MinHeap < BinaryHeap
       swap(position, smallest)
       bubble_down(smallest)
     end
+  end
+
+  def compare(a, b)
+    a < b
   end
 end
