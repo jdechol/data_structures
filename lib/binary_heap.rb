3471abb34bb266964f8ce7d9a923c88acc7d80ca
Jared Echols <jechols@mavenlink.com>
2022-10-03 11:27:14 -0600

    feat: now an element can be added to the heap

--- lib/binary_heap.rb
+++ lib/binary_heap.rb
@@ -1,6 +1,12 @@
 class BinaryHeap
+  attr_reader :data
+
   def initialize(data=[])
     @data = data
+  end
+
+  def add(element)
+    @data.push(element)
   end
 
   def empty?
--- spec/binary_heap_spec.rb
+++ spec/binary_heap_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe BinaryHeap do
     it "returns the element when there is only one element" do
       expect(described_class.new([1]).peak).to eq 1
     end
+
+    it "returns the max element when multiple elements are in the heap" do
+      data = [5,8,16,1,3]
+      expect(described_class.new(data).peak).to eq data.max
+    end
   end
 
   describe "#empty" do
@@ -20,5 +25,13 @@ RSpec.describe BinaryHeap do
     it "returns false when there are elements" do
       expect(described_class.new([1])).to_not be_empty
     end
-	end
+  end
+
+  describe "#add" do
+    it "can add an element when the heap is empty" do
+      heap = described_class.new
+      heap.add(1)
+      expect(heap.peak).to eq 1
+    end
+  end
 end
